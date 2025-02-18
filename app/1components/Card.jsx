13ff75f17219e0913d3eb324e6951244d4dcd371
kaavyarao05 <kaavyarao05@gmail.com>
2025-02-18 23:16:31 +0530

    feat: note css attempt

--- app/1components/Card.jsx
+++ app/1components/Card.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Card = ({title,preview}) => {
+const Card = ({title,preview,className}) => {
   return (
-    <div className='cardDiv'>
+    <div className={'cardDiv '+className}>
         <h2>{title}</h2>
         <p>{preview}</p>
     </div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,22 @@ import Card from '@/app/1components/Card';
 export default function Home() {
   return (
     <div>
-      <Card title={"Test Title"} preview={"Preview"}/>
+      <div>
+        <Card
+        title={"Test Title"}
+        preview={"Preview"}
+        className={"bg-[#ffcdd6]"}/>
+
+        <Card
+        title={"Test Title"}
+        preview={"Preview"}
+        className={"bg-[#9fffdf]"}/>
+        
+        <Card
+        title={"Test Title"}
+        preview={"Preview"}
+        className={"bg-[#a7e9ff]"}/>
+      </div>
     </div>
   );
 }
