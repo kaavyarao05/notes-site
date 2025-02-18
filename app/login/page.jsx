6811d89dddce9d7bc9b7959c9401eadab320ec9b
kaavyarao05 <kaavyarao05@gmail.com>
2025-02-18 22:09:11 +0530

    feat: login page structure

---
+++ app/login/page.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='login'>
+        <div>
+            <h2>Login</h2>
+            <label>
+                Username
+                <input className='user' id='user'></input>
+            </label>
+            <label>
+                Password
+                <input className='pass' id='pass'></input>
+            </label>
+            <button>Submit</button>
+        </div>
+    </div>
+  )
+}
+
+export default page
