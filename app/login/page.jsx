a14d7499022e89445a8c8489b36ceb0fde22e721
kaavyarao05 <kaavyarao05@gmail.com>
2025-02-22 13:45:45 +0530

    feat: redirect to login before signup

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,6 +1,28 @@
 "use client"
 
+import { useState } from "react";
+import { supabase } from "../lib/supabase"
+
 export default function LoginPage() {
+
+  const [data, setData]=useState({
+    email:"",
+    password:""
+  });
+  const login=async()=>{
+    try{
+      let {data,error}=await supabase
+    .auth
+    .signUp({
+      email:"kaavyarao05@gmail.com",
+      password:"password123"
+    })
+    }
+    catch(e){
+      console.log(e);
+    }
+    
+  }
  
   async function handleSubmit(event) {
     event.preventDefault()
@@ -15,7 +37,7 @@ export default function LoginPage() {
   return (
     <div>
       <div>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={login}>
           <input type="email" name="email" placeholder="Email" required />
           <input type="password" name="password" placeholder="Password" required />
           <button type="submit">Login</button>
